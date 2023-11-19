57e701cd460f800958c1aa69bb3c674f799865ea
nurbekomar <114141734+nurbekomar@users.noreply.github.com>
2023-11-19 14:25:13 +0600

    add HW1

---
+++ HW_1.py
@@ -0,0 +1,24 @@
+a = 5
+b = 10
+print('a + b =', a+b)
+print('a - b =', a-b)
+print('a / b =', a/b)
+print('a * b =', a*b)
+
+name = 'Nurbek'
+city = 'Shymkent'
+print(name + ' ' + city)
+
+r = 4
+pi = 3.14
+C = 2 * pi * r
+S = pi * r * r
+print('C =', C)
+print('S =', S)
+
+is_sunny = True
+temp = 27
+if is_sunny:
+    print(f'сегодня солнечный день и температура {temp}')
+else:
+    print(f'сегодня не солнечный день и температура {temp}')
