a605690870662f77b46a5c0fa2000d7a7e468195
nurbekomar <114141734+nurbekomar@users.noreply.github.com>
2023-11-20 16:12:38 +0600

    HW9(1-6)

---
+++ HW_9.py
@@ -0,0 +1,66 @@
+############ N1 #############
+# my_set1 = {1,23,34,4,5,6,7,67}
+# my_set2 = {1,33,44,46,5,67,7,67}
+
+# print('Разность ', my_set1.difference(my_set2))
+# print('Общие', my_set1.union(my_set2))
+
+############ N2 #############
+# inventory = {
+#     1: {'name': 'Яблока', 'amount': 10, 'price': 700},
+#     2: {'name': 'Банан', 'amount': 10, 'price': 500},
+#     3: {'name': 'Груша', 'amount': 10, 'price': 400}
+# }
+
+
+############ N3 #############
+# students = {
+#     'Smith': {'age': 20, 'course': 2, 'average grade': 4.5},
+#     'Johnson': {'age': 21, 'course': 3, 'average grade': 4.2},
+#     'Williams': {'age': 19, 'course': 1, 'average grade': 4.8},
+# }
+# print(students)
+# print('='*20)
+# delete_student = "Smith"
+# if delete_student in students:
+#     del students[delete_student]
+
+# print (students)
+
+
+############ N4 #############
+# def intersection_point(a, b):
+#     return a.intersection(b)
+
+# a = {'Tom', 'Bob', 'Alice'}
+# b = {'Tom', 'Jhon', 'Anna'}
+
+# print(intersection_point(a,b))
+
+############ N5 #############
+# inventory = {
+#     1: {'name': 'Яблока', 'amount': 10, 'price': 700},
+#     2: {'name': 'Банан', 'amount': 10, 'price': 500},
+#     3: {'name': 'Груша', 'amount': 10, 'price': 400}
+# }
+
+
+# def search_product(id):
+#     product = inventory.get(id)
+#     if product:
+#         return product
+#     else:
+#         return f'No {id} product'
+
+
+# print(search_product(1))
+
+
+############ N6 #############
+# set1 = set([1, 34, 56])
+# set2 = set([2, 4, 56])
+
+# print('intersection', set1.intersection(set2))
+# print('union', set1.union(set2))
+
+############ N7 #############
